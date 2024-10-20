41803f3165d3494b78264d65f81d5779dc1e32fe
Luiz Felipe <78387456+LuizWT@users.noreply.github.com>
2024-10-20 03:15:23 -0300

    Update sniper.py

--- tools/sniper.py
+++ tools/sniper.py
@@ -1,9 +1,9 @@
 from colorama import init, Fore, Style
 from functions.clear_terminal import clear_terminal
 from functions.create_output_file import execute_command_and_log
+from functions.proxy_chains import is_proxychains_enabled
 
 init(autoreset=True)
-
 
 def show_command_explanation(mode):
     explanations = {
@@ -23,140 +23,133 @@ def show_command_explanation(mode):
     print(explanations.get(mode, f"{Fore.RED}[INFO] {Style.BRIGHT}Modo não identificado."))
 
 def sniper(target, mode, additional_param=None):
-    base_command = "sudo sniper "
-    if mode == 'normal':
-        command = f"{base_command}-t {target}"
-    elif mode == 'osint_recon':
-        command = f"{base_command}-t {target} -o -re"
-    elif mode == 'stealth':
-        command = f"{base_command}-t {target} -m stealth -o -re"
-    elif mode == 'discover':
-        command = f"{base_command}-t {target} -m discover -w {additional_param}"
-    elif mode == 'port':
-        command = f"{base_command}-t {target} -m port -p {additional_param}"
-    elif mode == 'fullportonly':
-        command = f"{base_command}-t {target} -fp"
-    elif mode == 'web':
-        command = f"{base_command}-t {target} -m web"
-    elif mode == 'webporthttp':
-        command = f"{base_command}-t {target} -m webporthttp -p {additional_param}"
-    elif mode == 'webporthttps':
-        command = f"{base_command}-t {target} -m webporthttps -p {additional_param}"
-    elif mode == 'webscan':
-        command = f"{base_command}-t {target} -m webscan"
-    elif mode == 'bruteforce':
-        command = f"{base_command}-t {target} -b"
-    else:
-        command = None
+    base_command = "sudo sniper " if not is_proxychains_enabled() else "sudo proxychains sniper "
+    
+    commands = {
+        'normal': f"{base_command}-t {target}",
+        'osint_recon': f"{base_command}-t {target} -o -re",
+        'stealth': f"{base_command}-t {target} -m stealth -o -re",
+        'discover': f"{base_command}-t {target} -m discover -w {additional_param}",
+        'port': f"{base_command}-t {target} -m port -p {additional_param}",
+        'fullportonly': f"{base_command}-t {target} -fp",
+        'web': f"{base_command}-t {target} -m web",
+        'webporthttp': f"{base_command}-t {target} -m webporthttp -p {additional_param}",
+        'webporthttps': f"{base_command}-t {target} -m webporthttps -p {additional_param}",
+        'webscan': f"{base_command}-t {target} -m webscan",
+        'bruteforce': f"{base_command}-t {target} -b"
+    }
 
+    command = commands.get(mode)
     if command:
+        print(f"{Fore.CYAN}[INFO] Executando comando: {command}")
         execute_command_and_log(command, "sniper")
 
+
 def get_target():
-    return input(f"{Fore.GREEN}Digite o endereço do alvo (EX: 192.168.0.1 | site.com) ou [B] para voltar: ")
+    return input(f"{Fore.GREEN}Digite o endereço do alvo (EX: 192.168.0.1 | site.com) ou {Fore.RED}[B]{Fore.GREEN} para voltar: ")
 
 def get_cidr():
-    return input(f"{Fore.GREEN}Digite o CIDR (EX: 192.168.0.0/24) ou [B] para voltar: ")
+    return input(f"{Fore.GREEN}Digite o CIDR (EX: 192.168.0.0/24) ou {Fore.RED}[B]{Fore.GREEN} para voltar: ")
 
 def get_workspace():
-    return input(f"{Fore.GREEN}Digite o nome do Workspace ou [B] para voltar: ")
+    return input(f"{Fore.GREEN}Digite o nome do Workspace ou {Fore.RED}[B]{Fore.GREEN} para voltar: ")
 
 def get_port():
-    return input(f"{Fore.GREEN}Digite o número da porta ou [B] para voltar: ")
+    return input(f"{Fore.GREEN}Digite o número da porta ou {Fore.RED}[B]{Fore.GREEN} para voltar: ")
 
 def sniper_options(option):
     if option == "1":
         show_command_explanation('normal')
         target = get_target()
         if target.lower() == 'b':
-            clear_terminal() 
+            clear_terminal()
             return  
         sniper(target, 'normal')
     elif option == "2":
         show_command_explanation('osint_recon')
         target = get_target()
         if target.lower() == 'b':
-            clear_terminal() 
+            clear_terminal()
             return  
         sniper(target, 'osint_recon')
     elif option == "3":
         show_command_explanation('stealth')
         target = get_target()
         if target.lower() == 'b':
-            clear_terminal() 
+            clear_terminal()
             return  
         sniper(target, 'stealth')
     elif option == "4":
         show_command_explanation('discover')
         cidr = get_cidr()
         if cidr.lower() == 'b':
-            clear_terminal() 
+            clear_terminal()
             return  
         workspace = get_workspace()
         if workspace.lower() == 'b':
-            clear_terminal() 
+            clear_terminal()
             return  
         sniper(cidr, 'discover', workspace)
     elif option == "5":
         show_command_explanation('port')
         target = get_target()
         if target.lower() == 'b':
-            clear_terminal() 
+            clear_terminal()
             return  
         port = get_port()
         if port.lower() == 'b':
-            clear_terminal() 
+            clear_terminal()
             return  
         sniper(target, 'port', port)
     elif option == "6":
         show_command_explanation('fullportonly')
         target = get_target()
         if target.lower() == 'b':
-            clear_terminal() 
+            clear_terminal()
             return  
         sniper(target, 'fullportonly')
     elif option == "7":
         show_command_explanation('web')
         target = get_target()
         if target.lower() == 'b':
-            clear_terminal() 
+            clear_terminal()
             return  
         sniper(target, 'web')
     elif option == "8":
         show_command_explanation('webporthttp')
         target = get_target()
         if target.lower() == 'b':
-            clear_terminal() 
+            clear_terminal()
             return  
         port = get_port()
         if port.lower() == 'b':
-            clear_terminal() 
+            clear_terminal()
             return  
         sniper(target, 'webporthttp', port)
     elif option == "9":
         show_command_explanation('webporthttps')
         target = get_target()
         if target.lower() == 'b':
-            clear_terminal() 
+            clear_terminal()
             return  
         port = get_port()
         if port.lower() == 'b':
-            clear_terminal() 
+            clear_terminal()
             return  
         sniper(target, 'webporthttps', port)
     elif option == "10":
         show_command_explanation('webscan')
         target = get_target()
         if target.lower() == 'b':
-            clear_terminal() 
+            clear_terminal()
             return  
         sniper(target, 'webscan')
     elif option == "11":
         show_command_explanation('bruteforce')
         target = get_target()
         if target.lower() == 'b':
-            clear_terminal() 
-            return 
+            clear_terminal()
+            return  
         sniper(target, 'bruteforce')
 
 def sniper_menu_loop():
