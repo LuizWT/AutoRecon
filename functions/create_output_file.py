0994998b4794a516e2ac76d06ee97614f090ab83
Luiz Felipe <78387456+LuizWT@users.noreply.github.com>
2024-10-19 22:41:04 -0300

    Update create_output_file.py

--- functions/create_output_file.py
+++ functions/create_output_file.py
@@ -3,16 +3,15 @@ from colorama import init, Fore, Style
 
 init(autoreset=True)
 
-def create_output_file():
+def create_output_file(script_name):
     output_dir = "output"
 
-    script_name = os.path.basename(__file__).replace('.py', '')
     output_file = os.path.join(output_dir, f"{script_name}_output.txt")
     os.makedirs(output_dir, exist_ok=True)
     return output_file
 
-def execute_command_and_log(command):
-    output_file = create_output_file()
+def execute_command_and_log(command, script_name):
+    output_file = create_output_file(script_name)
     full_command = f"{command} >> {output_file} 2>&1"
     print(f"{Fore.YELLOW}Executando comando: {full_command}")
     os.system(full_command)
