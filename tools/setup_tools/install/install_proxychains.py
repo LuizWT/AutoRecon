9a4fb364aba2b69019be9b65bc6a6c73801ae326
Luiz Felipe <78387456+LuizWT@users.noreply.github.com>
2024-10-23 22:06:24 -0300

    Update install_proxychains.py

--- tools/setup_tools/install/install_proxychains.py
+++ tools/setup_tools/install/install_proxychains.py
@@ -1,5 +1,4 @@
 import os
-import platform
 from colorama import init, Fore
 
 init(autoreset=True)
@@ -8,20 +7,19 @@ def install_proxychains():
     try:
         print(f"{Fore.CYAN}[INFO] Iniciando a instalação do ProxyChains...")
         
-        if platform.system() == 'Linux':
-            if os.path.isfile('/etc/debian_version'):
-                os.system('sudo apt-get update && sudo apt-get install -y proxychains-ng')
-            elif os.path.isfile('/etc/redhat-release'):
-                os.system('sudo dnf install -y proxychains-ng')
-            elif os.path.isfile('/etc/arch-release'):
-                os.system('sudo pacman -Syu --noconfirm proxychains-ng')
-            elif os.path.isfile('/etc/SuSE-release'):
-                os.system('sudo zypper refresh && sudo zypper install -y proxychains-ng')
-            else:
-                print(f"{Fore.RED}[ERROR] Distribuição não suportada para instalação do ProxyChains.")
-                return
-            print(f"{Fore.CYAN}[INFO] ProxyChains instalado com sucesso.")
+        if os.path.isfile('/etc/debian_version'):
+            os.system('sudo apt-get update && sudo apt-get install -y proxychains-ng')
+        elif os.path.isfile('/etc/redhat-release'):
+            os.system('sudo dnf install -y proxychains-ng')
+        elif os.path.isfile('/etc/arch-release'):
+            os.system('sudo pacman -Syu --noconfirm proxychains-ng')
+        elif os.path.isfile('/etc/SuSE-release'):
+            os.system('sudo zypper refresh && sudo zypper install -y proxychains-ng')
         else:
-            print(f"{Fore.RED}[ERROR] Este script só suporta instalação no Linux.")
+            print(f"{Fore.RED}[ERROR] Distribuição não suportada para instalação do ProxyChains.")
+            return
+
+        print(f"{Fore.CYAN}[INFO] ProxyChains instalado com sucesso.")
+        
     except Exception as e:
-        print(f"{Fore.RED}[ERROR] Ocorreu um erro durante a instalação: {e}")
+        print(f"{Fore.RED}[ERROR] Ocorreu um erro durante a instalação do ProxyChains: {e}")
