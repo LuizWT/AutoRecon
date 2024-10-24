998ffd143f9326e451a9f9c209365917803b3150
Luiz Felipe <78387456+LuizWT@users.noreply.github.com>
2024-10-23 22:07:00 -0300

    Update install_sniper.py

--- tools/setup_tools/install/install_sniper.py
+++ tools/setup_tools/install/install_sniper.py
@@ -1,5 +1,4 @@
 import os
-import platform
 from colorama import init, Fore
 
 init(autoreset=True)
@@ -7,13 +6,14 @@ init(autoreset=True)
 def install_sniper():
     try:
         print(f"{Fore.CYAN}[INFO] Iniciando a instalação do Sn1per...")
-        if platform.system() == 'Linux':
-            if not os.path.exists('Sn1per'):
-                os.system('git clone https://github.com/1N3/Sn1per')
-            os.chdir('Sn1per')
-            os.system('sudo bash install.sh')
-            print(f"{Fore.CYAN}[INFO] Sn1per instalado com sucesso.")
-        else:
-            print(f"{Fore.RED}[ERROR] Este script só suporta instalação no Linux.")
+
+        if not os.path.exists('Sn1per'):
+            os.system('git clone https://github.com/1N3/Sn1per')
+        
+        os.chdir('Sn1per')
+        os.system('sudo bash install.sh')
+
+        print(f"{Fore.CYAN}[INFO] Sn1per instalado com sucesso.")
+        
     except Exception as e:
-        print(f"{Fore.RED}[ERROR] Ocorreu um erro durante a instalação: {e}")
+        print(f"{Fore.RED}[ERROR] Ocorreu um erro durante a instalação do Sn1per: {e}")
