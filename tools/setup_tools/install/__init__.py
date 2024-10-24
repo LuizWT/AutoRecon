63de3f39a4c7147367d0e9460ce67f046950835d
Luiz Felipe <78387456+LuizWT@users.noreply.github.com>
2024-10-23 22:28:20 -0300

    Delete tools/setup_tools directory

--- tools/setup_tools/install/__init__.py
+++
@@ -1,5 +0,0 @@
-from .install_nmap import install_nmap
-from .install_proxychains import install_proxychains
-from .install_ruby import install_ruby
-from .install_sniper import install_sniper
-from .install_wpscan import install_wpscan
