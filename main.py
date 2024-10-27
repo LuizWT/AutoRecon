f78ad03c9d1377f9452e43e009f8cbe9314145b4
Luiz Felipe <78387456+LuizWT@users.noreply.github.com>
2024-10-27 03:05:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from functions.check_system import check_system
 from functions.clear_terminal import clear_terminal
-from setup_tools.setup import check_nmap, check_wpscan, check_sniper, check_ruby, check_proxychains
-from setup_tools.setup import install_nmap, install_wpscan, install_sniper, install_ruby, install_proxychains
+from setup_tools.setup import check_nmap, check_wpscan, check_sniper, check_ruby, check_proxychains, check_go, check_nuclei, check_nikto
+from setup_tools.setup import install_nmap, install_wpscan, install_sniper, install_ruby, install_proxychains, install_go, install_nuclei, install_nikto
 from tools.sniper import sniper_menu_loop
 from tools.nmap import nmap_menu_loop
 from tools.wpscan import wpscan_menu_loop
+from tools.nuclei import nuclei_menu_loop
+from tools.nikto import nikto_menu_loop
 from functions.proxy_chains import toggle_proxychains, proxychains_enabled, check_proxychains_installed
 from colorama import init, Fore, Style
+import readline
 
 init(autoreset=True)  # inicia o colorama
 
@@ -27,6 +30,8 @@ def main_menu():
     {Fore.CYAN}[1] {Fore.RESET}SNIPER
     {Fore.CYAN}[2] {Fore.RESET}NMAP
     {Fore.CYAN}[3] {Fore.RESET}WPSCAN
+    {Fore.CYAN}[4] {Fore.RESET}NUCLEI
+    {Fore.CYAN}[5] {Fore.RESET}NIKTO
     {Fore.CYAN}[0] {Fore.RESET}ProxyChains [{status_color}{status_text}{Fore.RESET}] {proxychains_info}
     {Fore.RED}[9] {Fore.RESET}Sair
     """)
@@ -99,6 +104,41 @@ def main():
                         install_wpscan()
                     else:
                         print(f"{Fore.RED}[INFO] Retornando ao menu principal...")
+        elif option == "4":
+            clear_terminal()
+            print(f"{Fore.GREEN}[INFO] Verificando a instalação do Nuclei...")
+            if check_nuclei(): # Nuclei DEVE ser verificado primeiro
+                print(f"{Fore.GREEN}[INFO] Abrindo o Nuclei...")
+                clear_terminal()
+                nuclei_menu_loop()
+            else:
+                if check_go():
+                    install_nuclei_choice = input(f"{Fore.YELLOW}[INFO] Deseja instalar o Nuclei? (y/n): ").lower()
+                    if install_nuclei_choice in ['s', 'y']:
+                        install_nuclei()
+                    else:
+                        print(f"{Fore.RED}[INFO] Retornando ao menu principal...")
+                else:
+                    install_go_choice = input(f"{Fore.YELLOW}[INFO] GO não está instalado. Deseja instalar GO e Nuclei? (y/n): ").lower()
+                    if install_go_choice in ['s', 'y']:
+                        install_go()
+                        install_nuclei()
+        elif option == "5":
+            clear_terminal()
+            print(f"{Fore.GREEN}[INFO] Verificando a instalação do Nikto...")
+            if check_nikto():
+                print(f"{Fore.GREEN}[INFO] Abrindo o menu NIKTO...")
+                clear_terminal()
+                nikto_menu_loop()
+            else:
+                install = input(f"{Fore.YELLOW}[INFO] Nikto não está instalado. Deseja instalar o Nikto? (y/n): ").lower()
+                if install in ['s', 'y']:
+                    install_nikto()
+                    print(f"{Fore.GREEN}[INFO] Abrindo o menu NIKTO...")
+                    clear_terminal()
+                    nikto_menu_loop()
+                else:
+                    print(f"{Fore.RED}[INFO] Retornando ao menu principal...")
         elif option == "0":
             clear_terminal()
             print(f"{Fore.GREEN}[INFO] Verificando a instalação do ProxyChains...")
