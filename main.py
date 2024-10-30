bd9b7604f5505e56540bd07024dcc0bf80212cb5
Luiz Felipe <78387456+LuizWT@users.noreply.github.com>
2024-10-30 11:39:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-# Importações de bibliotecas padrão
 import readline
-
-# Importações de bibliotecas de terceiros
 from colorama import init, Fore, Style
+from prompt_toolkit import PromptSession
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.formatted_text import HTML
+import asyncio
 
-# Importações de módulos locais
 from functions.check_system import check_system
 from functions.clear_terminal import clear_terminal
 from functions.check_and_install_tool import check_and_install_tool
+from functions.set_global_target import set_global_target, bindings, state 
 
 from setup_tools.setup import (
     check_nmap, check_wpscan, check_sniper, check_ruby, check_proxychains, check_go, check_nuclei, check_nikto,
@@ -21,17 +22,22 @@ from tools.nuclei import nuclei_menu_loop
 from tools.nikto import nikto_menu_loop
 from functions.proxy_chains import toggle_proxychains, proxychains_enabled, check_proxychains_installed
 
-init(autoreset=True)  # inicia o colorama
+init(autoreset=True)
+
+session = PromptSession()
 
 def main_menu():
     proxychains_info = " (/etc/proxychains.conf)" if check_proxychains_installed() else ""
     status_color = Fore.GREEN if proxychains_enabled else Fore.RED
     status_text = "ON" if proxychains_enabled else "OFF"
+    
+    global_target_display = f"Alvo: {state['global_target']}" if state['global_target'] else "Alvo: Não definido"
+    
     print(rf"""
     {Fore.BLUE}{Style.BRIGHT}
                 _        _____                      
-     /\        | |      |  __ \                     
-    /  \  _   _| |_ ___ | |__) |___  ___ ___  _ __  
+     /\        | |      |  __ \      Pressione Ctrl+T para definir o alvo               
+    /  \  _   _| |_ ___ | |__) |___  ___ ___  _ __ {Fore.YELLOW}{global_target_display}{Fore.BLUE}{Style.BRIGHT}
    / /\ \| | | | __/ _ \|  _  // _ \/ __/ _ \| '_ \ 
   / ____ \ |_| | || (_) | | \ \  __/ (_| (_) | | | |
  /_/    \_\__,_|\__\___/|_|  \_\___|\___\___/|_| |_|
@@ -46,7 +52,8 @@ def main_menu():
     {Fore.RED}[9] {Fore.RESET}Sair
     """)
 
-def main():
+
+async def main_loop():
     global proxychains_enabled
 
     if not check_system():
@@ -55,7 +62,8 @@ def main():
     while True:
         clear_terminal()
         main_menu()
-        option = input(f"{Fore.YELLOW}Escolha uma opção: ")
+
+        option = await session.prompt_async(HTML(f"<ansiyellow>Escolha uma opção:</ansiyellow> "), key_bindings=bindings)
 
         if option == "9":
             clear_terminal()
@@ -63,19 +71,20 @@ def main():
             break
         elif option == "1":
             clear_terminal()
-            check_and_install_tool("Sn1per", check_sniper, install_sniper, sniper_menu_loop)
+            check_and_install_tool("Sn1per", check_sniper, install_sniper, sniper_menu_loop, state['global_target'])
         elif option == "2":
             clear_terminal()
-            check_and_install_tool("Nmap", check_nmap, install_nmap, nmap_menu_loop)
+            check_and_install_tool("Nmap", check_nmap, install_nmap, nmap_menu_loop, state['global_target'])
         elif option == "3":
             clear_terminal()
-            check_and_install_tool("WPScan", check_wpscan, install_wpscan, wpscan_menu_loop, dep_check_func=check_ruby, dep_install_func=install_ruby)
+            check_and_install_tool("WPScan", check_wpscan, install_wpscan, wpscan_menu_loop, state['global_target'], dep_check_func=check_ruby, dep_install_func=install_ruby)
         elif option == "4":
             clear_terminal()
-            check_and_install_tool("Nuclei", check_nuclei, install_nuclei, nuclei_menu_loop, dep_check_func=check_go, dep_install_func=install_go)
+            check_and_install_tool("Nuclei", check_nuclei, install_nuclei, nuclei_menu_loop, state, dep_check_func=check_go, dep_install_func=install_go)
         elif option == "5":
             clear_terminal()
-            check_and_install_tool("Nikto", check_nikto, install_nikto, nikto_menu_loop)
+            check_and_install_tool("Nikto", check_nikto, install_nikto, nikto_menu_loop, state['global_target'])
+
         elif option == "0":
             clear_terminal()
             print(f"{Fore.GREEN}[INFO] Verificando a instalação do ProxyChains...")
@@ -95,4 +104,4 @@ def main():
                     print(f"{Fore.RED}[INFO] Retornando ao menu principal...")
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main_loop())
