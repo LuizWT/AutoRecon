4776a8702f1bdda0ac79c0e278b32d784d7ea771
Luiz Felipe <78387456+LuizWT@users.noreply.github.com>
2024-10-30 11:38:55 -0300

    Update wpscan.py

--- tools/wpscan.py
+++ tools/wpscan.py
@@ -2,19 +2,21 @@ from colorama import init, Fore, Style
 from functions.clear_terminal import clear_terminal
 from functions.create_output_file import execute_command_and_log
 from functions.proxy_chains import is_proxychains_enabled
+from functions.set_global_target import state
+from functions.toggle_info import toggle_info, is_info_visible
 
-init(autoreset=True)
+init(autoreset=True)  # inicia o colorama
 
-def show_command_explanation(mode):
+def get_command_explanation(mode):
     explanations = {
-        'normal': f"{Fore.CYAN}[INFO] {Style.BRIGHT}Normal Mode: Realiza uma varredura padrão em busca de vulnerabilidades no WordPress.",
-        'enumerate_users': f"{Fore.CYAN}[INFO] {Style.BRIGHT}Enumerate Users: Tenta descobrir os usuários do WordPress.",
-        'enumerate_plugins': f"{Fore.CYAN}[INFO] {Style.BRIGHT}Enumerate Plugins: Lista os plugins instalados e suas vulnerabilidades conhecidas.",
-        'enumerate_themes': f"{Fore.CYAN}[INFO] {Style.BRIGHT}Enumerate Themes: Lista os temas instalados e suas vulnerabilidades conhecidas.",
-        'scan': f"{Fore.CYAN}[INFO] {Style.BRIGHT}Scan Mode: Realiza uma varredura completa em busca de vulnerabilidades.",
+        'normal': f"{Fore.CYAN}| [INFO] {Fore.BLUE} Realiza uma varredura padrão em busca de vulnerabilidades no WordPress.",
+        'enumerate_users': f"{Fore.CYAN}| [INFO] {Fore.BLUE} Tenta descobrir os usuários do WordPress.",
+        'enumerate_plugins': f"{Fore.CYAN}| [INFO] {Fore.BLUE} Lista os plugins instalados e suas vulnerabilidades conhecidas.",
+        'enumerate_themes': f"{Fore.CYAN}| [INFO] {Fore.BLUE} Lista os temas instalados e suas vulnerabilidades conhecidas.",
+        'scan': f"{Fore.CYAN}| [INFO] {Fore.BLUE} Realiza uma varredura completa em busca de vulnerabilidades.",
     }
+    return explanations.get(mode, f"{Fore.RED}| [INFO] Modo não identificado.")
 
-    print(explanations.get(mode, f"{Fore.RED}[INFO] {Style.BRIGHT}Modo não identificado."))
 
 def wpscan(target, mode, api_token=None):
     base_command = "wpscan" if not is_proxychains_enabled() else "proxychains4 wpscan"
@@ -24,88 +26,72 @@ def wpscan(target, mode, api_token=None):
         'enumerate_users': f"{base_command} --url {target} --enumerate u",
         'enumerate_plugins': f"{base_command} --url {target} --enumerate p",
         'enumerate_themes': f"{base_command} --url {target} --enumerate t",
-        'scan': f"{base_command} --url {target} --api-token {api_token}"  # Usa o token aqui
+        'scan': f"{base_command} --url {target} --api-token {api_token}"
     }
 
     command = commands.get(mode)
     if command:
-        print(f"{Fore.CYAN}[INFO] Executando comando: {command}")
         execute_command_and_log(command, "wpscan")
 
-def get_target():
-    return input(f"{Fore.GREEN}Digite o endereço do alvo (EX: 192.168.0.1 | site.com) ou {Fore.RED}[B]{Fore.GREEN} para voltar: ")
+def get_target(global_target):
+    return global_target if global_target else input(f"{Fore.RED}Digite o alvo ou [B] para voltar: ")
 
 def get_api_token():
     return input(f"{Fore.GREEN}Digite seu API Token ou {Fore.RED}[B]{Fore.GREEN} para voltar: ")
 
-def wpscan_options(option):
+def wpscan_options(option, global_target):
+    target = get_target(global_target)
+    if target.lower() == 'b':
+        clear_terminal()
+        return  
+    
     if option == "1":
-        show_command_explanation('normal')
-        target = get_target()
-        if target.lower() == 'b':
-            clear_terminal()
-            return  
         wpscan(target, 'normal')
     elif option == "2":
-        show_command_explanation('enumerate_users')
-        target = get_target()
-        if target.lower() == 'b':
-            clear_terminal()
-            return  
         wpscan(target, 'enumerate_users')
     elif option == "3":
-        show_command_explanation('enumerate_plugins')
-        target = get_target()
-        if target.lower() == 'b':
-            clear_terminal()
-            return  
         wpscan(target, 'enumerate_plugins')
     elif option == "4":
-        show_command_explanation('enumerate_themes')
-        target = get_target()
-        if target.lower() == 'b':
-            clear_terminal()
-            return  
         wpscan(target, 'enumerate_themes')
     elif option == "5":
-        show_command_explanation('scan')
-        target = get_target()
-        if target.lower() == 'b':
-            clear_terminal()
-            return  
         api_token = get_api_token()  # Solicita o token aqui
         if api_token.lower() == 'b':
             clear_terminal()
             return  
         wpscan(target, 'scan', api_token)
 
-def wpscan_menu_loop():
+def wpscan_menu_loop(global_target):
     while True:
+        clear_terminal()
+        global_target_display = f"Alvo: {state['global_target']}" if state['global_target'] else "Alvo: Não definido"
+
         print(rf"""
         {Fore.BLUE}
-        __          _______   _____  _____          _   _ 
+        __          _______   _____  _____          _   _ {Fore.YELLOW}{global_target_display}{Fore.BLUE}
         \ \        / /  __ \ / ____|/ ____|   /\   | \ | |
          \ \  /\  / /| |__) | (___ | |       /  \  |  \| |
           \ \/  \/ / |  ___/ \___ \| |      / /\ \ | . ` |
            \  /\  /  | |     ____) | |____ / ____ \| |\  |
             \/  \/   |_|    |_____/ \_____/_/    \_\_| \_|
         
-        {Fore.CYAN}[1] {Fore.RESET}Modo Normal
-        {Fore.CYAN}[2] {Fore.RESET}Enumerar Usuários
-        {Fore.CYAN}[3] {Fore.RESET}Enumerar Plugins
-        {Fore.CYAN}[4] {Fore.RESET}Enumerar Temas
-        {Fore.CYAN}[5] {Fore.RESET}Scan Completo
+        {Fore.CYAN}[1] {Fore.RESET}Modo Normal {get_command_explanation("normal") if is_info_visible() else ""}
+        {Fore.CYAN}[2] {Fore.RESET}Enumerar Usuários {get_command_explanation("enumerate_users") if is_info_visible() else ""}
+        {Fore.CYAN}[3] {Fore.RESET}Enumerar Plugins {get_command_explanation("enumerate_plugins") if is_info_visible() else ""}
+        {Fore.CYAN}[4] {Fore.RESET}Enumerar Temas {get_command_explanation("enumerate_themes") if is_info_visible() else ""}
+        {Fore.CYAN}[5] {Fore.RESET}Scan Completo {get_command_explanation("scan") if is_info_visible() else ""}
         {Fore.RED}[B] {Fore.RESET}Voltar
+        {Fore.YELLOW}[I] {Fore.RESET}Alternar Informações
         """)
 
         option = input(f"{Fore.YELLOW}Escolha uma opção: ")
-        clear_terminal()
-    
+        
         if option.lower() == 'b':
             break
+        elif option.lower() == 'i':
+            toggle_info()
+            continue
 
         if option in [str(i) for i in range(1, 6)]:
-            wpscan_options(option)
+            wpscan_options(option, global_target)
         else:
-            clear_terminal()
             print(f"{Fore.RED}Opção inválida.")
