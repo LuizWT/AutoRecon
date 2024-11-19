ac50d7a2c3fc171fb47447907dddb45d042bd5e9
LuizWT <xluiz452@gmail.com>
2024-11-19 00:47:02 -0300

    Refatora a validação de URL para wpscan

--- functions/validate_protocol.py
+++ functions/validate_protocol.py
@@ -3,14 +3,25 @@ import re
 init(autoreset=True)
 
 #TODO Resolver bug: caso a entrada seja "www.exemplo", a validação de extensão (.com, .gov.br, etc) não ocorre
+
+from colorama import Fore, init
+import re
+init(autoreset=True)
+
+# Função principal de validação da URL
 def validate_url(url):
 
+    if is_valid_url(url):
+        return url
+    
+    # Remove o www. antes de validar a extensão do domínio
     url = validate_domain_extension(url)
     
     domain = url.split("://")[-1].split("/")[0]
     if domain.startswith("www."):
         domain = domain[4:]  # Remove o www.
 
+    # Se o URL não começar com http:// ou https://, pede para o usuário escolher um protocolo
     if not url.startswith(('http://', 'https://')):
         print(f"{Fore.YELLOW}O URL não contém protocolo. Escolha qual protocolo será usado:\n{Fore.CYAN}[1] {Fore.RESET}https://\n{Fore.CYAN}[2] {Fore.RESET}http://")
         
@@ -27,10 +38,16 @@ def validate_url(url):
     
     return url
 
+# Função para validar a extensão do domínio
 def validate_domain_extension(url):
     # Remoção do protocolo HTTP ou HTTPS para melhor funcionamento do REGEX
     domain = url.split("://")[-1].split("/")[0]
+    
+    # Se o domínio começar com www., remove
+    if domain.startswith("www."):
+        domain = domain[4:]
 
+    # Verifica se a extensão do domínio está no formato correto
     if not re.search(r'\.\w{2,}(\.\w{2,})*$', domain):
         print(f"{Fore.YELLOW}O domínio fornecido não contém uma extensão válida.\n\nExemplos de extensões:\n{Fore.GREEN}.com, .org,\n.net, .br,\n.io, .edu")
 
@@ -44,5 +61,10 @@ def validate_domain_extension(url):
             else:
                 print(f"{Fore.RED}A extensão fornecida é inválida. Tente novamente.")
 
-    # URL com o domínio corrigido (sem protocolo por enquanto)
+    # Retorna o domínio corrigido, sem o protocolo por enquanto
     return f"{domain}"
+
+def is_valid_url(url):
+    # Verifica se a URL tem um protocolo e uma extensão válida
+    pattern = r'^(https?://)?(www\.)?[\w-]+\.[a-z]{2,}(\.[a-z]{2,})*(/.*)?$'
+    return bool(re.match(pattern, url))
