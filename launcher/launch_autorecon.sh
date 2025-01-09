ad2ffdab10f9a3780c92b36a51f0656f43838a14
luizwt <luizfelipe.sec@proton.me>
2025-01-08 22:35:07 -0300

    Garante uma compatibilidade mais abrangente de terminais

--- launcher/launch_autorecon.sh
+++ launcher/launch_autorecon.sh
@@ -11,6 +11,14 @@ elif command -v alacritty &>/dev/null; then
     alacritty -e bash -c "cd $AUTORECON_DIR && sudo python3 autorecon.py; exec bash"
 elif command -v kitty &>/dev/null; then
     kitty bash -c "cd $AUTORECON_DIR && sudo python3 autorecon.py; exec bash"
+elif command -v tilix &>/dev/null; then
+    tilix -e "bash -c 'cd $AUTORECON_DIR && sudo python3 autorecon.py; exec bash'"
+elif command -v lxterminal &>/dev/null; then
+    lxterminal -e "bash -c 'cd $AUTORECON_DIR && sudo python3 autorecon.py; exec bash'"
+elif command -v xterm &>/dev/null; then
+    xterm -e "bash -c 'cd $AUTORECON_DIR && sudo python3 autorecon.py; exec bash'"
+elif command -v terminator &>/dev/null; then
+    terminator -e "bash -c 'cd $AUTORECON_DIR && sudo python3 autorecon.py; exec bash'"
 else
     echo "Nenhum terminal compatível foi encontrado."
     exit 1
